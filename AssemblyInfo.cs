25ae6791084b01cb65b2119e23e3bd71a99df5f1
d3solta1on <susik2017ruslan@gmail.com>
2026-03-18 20:39:09 +0400

    Обновление структуры проекта: добавлен файл QAMP.csproj и исправлены ссылки в решении

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,10 +1,10 @@
 using System.Windows;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
-                                                //(used if a resource is not found in the page,
-                                                // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly   //where the generic resource dictionary is located
-                                                //(used if a resource is not found in the page,
-                                                // app, or any theme specific resource dictionaries)
-)]
+[assembly: AssemblyTitle("QAMP")]
+[assembly: AssemblyDescription("QAMP - Quick Attractive Media Player")] 
+[assembly: AssemblyProduct("QAMP")]
+
+
